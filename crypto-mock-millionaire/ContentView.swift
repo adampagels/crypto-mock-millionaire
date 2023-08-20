f0ea2141cc457629d1a411b59d132fdf598d914b
Adam Pagels <pagels@hotmail.ca>
2023-08-20 18:38:16 +0200

    Create function to fetch cryptocurrency price history

--- crypto-mock-millionaire/ContentView.swift
+++ crypto-mock-millionaire/ContentView.swift
@@ -30,8 +30,6 @@ struct ContentView: View {
     }
 }
 
-
-
 func getCoins() async throws -> [Coin] {
     let endpoint = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false"
     
--- crypto-mock-millionaire/CryptocurrencyDetails.swift
+++ crypto-mock-millionaire/CryptocurrencyDetails.swift
@@ -1,21 +1,53 @@
-//
-//  CryptocurrencyDetails.swift
-//  crypto-mock-millionaire
-//
-//  Created by Adam Pagels on 2023-08-20.
-//
-
 import SwiftUI
 
 struct CryptocurrencyDetails: View {
+    @State private var cryptocurrencyPriceHistory: PriceHistory = PriceHistory(prices: [])
     let cryptocurrency: String
-
+    
     var body: some View {
-        if !cryptocurrency.isEmpty {
-            Text(cryptocurrency)
-        } else {
-            Text("Bitcoin")
+        VStack() {
+            if !cryptocurrency.isEmpty {
+                Text(cryptocurrency)
+            } else {
+                Text("Bitcoin")
+            }
         }
+        .task {
+            do {
+                cryptocurrencyPriceHistory = try await getCryptocurrencyPriceHistory()
+                print(cryptocurrencyPriceHistory)
+            } catch PriceHistoryError.invalidResponse{
+                print("invalid response")
+            } catch PriceHistoryError.invalidData{
+                print("invalid data")
+            } catch PriceHistoryError.invalidURL{
+                print("invalid url")
+            } catch {
+                print("unexpected error")
+            }
+        }
+    }
+}
+
+func getCryptocurrencyPriceHistory() async throws -> PriceHistory {
+    // TODO: Use cryptocurrency variable rather than hardcoded name
+    let endpoint = "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=365"
+    
+    guard let url = URL(string: endpoint) else {
+        throw PriceHistoryError.invalidURL
+    }
+    
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+        throw PriceHistoryError.invalidResponse
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(PriceHistory.self, from: data)
+    } catch {
+        throw PriceHistoryError.invalidData
     }
 }
 
@@ -23,4 +55,14 @@ struct CryptocurrencyDetails_Previews: PreviewProvider {
     static var previews: some View {
         CryptocurrencyDetails(cryptocurrency: String())
     }
+}
+
+struct PriceHistory: Codable {
+    let prices: [[Double]]
+}
+
+enum PriceHistoryError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
 }
